b575e41e4a6235ca5c9df06f401b22195d28ea36
Bayu Darmawan <darmawanbayu1@gmail.com>
2020-09-23 21:33:53 +0700

    update
    testing buat home sama header

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import Header from '../../components/Header'
+import './home.css'
+class Home extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <div>
+                <Header/>
+              <div className="home-div">
+                        <div className="img-1">
+                            <img src="" alt=""/>
+                        </div>  
+              </div>
+                
+
+
+            </div>
+         );
+    }
+}
+ 
+export default Home;
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+    state = {  }
+    render() { 
+        return (
+            <div>INI LOGIN</div>
+          );
+    }
+}
+ 
+export default Login;
