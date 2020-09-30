78ba665195f1763afa150264995f82317446cfa0
Bayu Darmawan <darmawanbayu1@gmail.com>
2020-09-30 15:46:01 +0700

    balikin jadi array

--- src/pages/detailProduct/detailProd.jsx
+++ src/pages/detailProduct/detailProd.jsx
@@ -11,7 +11,7 @@ import {Link} from 'react-router-dom'
 import {connect} from 'react-redux';
 class DetailProduct extends Component {
     state = { 
-        product:{},
+        product:[],
         dataParse:{},
         productThunk:[]
      }
