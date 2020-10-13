5feead406a6501d8e71c883a9beaf11615960697
Bayu Darmawan <darmawanbayu1@gmail.com>
2020-10-13 18:06:59 +0700

    update payment

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -16,6 +16,7 @@ import {Redirect} from 'react-router-dom'
 import Swal from 'sweetalert2'
 import withReactContent from 'sweetalert2-react-content'
 import {toast} from 'react-toastify'  
+import { CodeSharp } from '@material-ui/icons';
 const MySwal = withReactContent(Swal)
 
 class Payment extends Component {
@@ -140,77 +141,183 @@ class Payment extends Component {
      onbayarpakebukti=()=>{
         console.log(this.props.id)
         console.log(this.state.bukti.current.value)
-        Axios.post(`http://localhost:5001/cart/addTransactions`,{
-            status:'WaitingAdmin',
+        var sqlcart=this.state.sqlCart.map((val)=>{
+            return {
+                ProductId:val.ProductId,
+                harga:val.harga,
+                Qty:val.Qty
+            }
+        })
+        Axios.post(`http://localhost:5001/cart/addNewTransactions`,{
             userId:this.props.id,
             tanggalPembayaran:new Date().getTime(),
-            metode:'upload',
-            buktiPembayaran:this.state.bukti.current.value
+            buktiPembayaran:this.state.bukti.current.value,
+            sqlCart:sqlcart
         }).then((res)=>{
-            console.log(res.data)  
-            // console.log(res.data[0].id)
-
-                var arr=[]
-                this.state.sqlCart.forEach((val)=>{
-                    console.log(val)
-                    console.log(res.data[0].id)
-                    console.log(val.ProductId)
-                    console.log(val.harga)
-                    console.log(val.Qty)
-                    arr.push(Axios.post(`http://localhost:5001/cart/addTransactDetail`,{
-                        transactionId:res.data[0].id,
-                        productId:val.ProductId,
-                        price:parseInt(val.harga),
-                        qty:val.Qty
-                    }).then((res)=>{
-                        console.log(res.data)
-                    }).catch((err)=>{
-                        console.log(err)
-                    }))
-                })
-
-                Axios.all(arr).then((res1)=>{
-                    var deleteArr=[]
-                    this.state.sqlCart.forEach((val)=>{
-                        deleteArr.push(Axios.delete(`http://localhost:5001/cart/deleteCartTransact/${this.state.idUser}`))
-                    })
-                    Axios.all(deleteArr)
-                    .then(()=>{
-                            Axios.get(`http://localhost:5001/cart/allQty/${this.state.idUser}`)
-                            .then((res3)=>{
-                                this.setState({sqlCart:res3.data,isOpen:false})
-                                toast.error(`Waiting Admin For Accepting Your Payment! THANKYOU`, {
-                                    position: "top-right",
-                                    autoClose: 2000,
-                                    hideProgressBar: false,
-                                    closeOnClick: true,
-                                    draggable: true,
-                                    progress: undefined,
-                                });
-                                this.setState({successful:true})
-                            }).catch((err)=>{
-                                console.log(err)
-                            })
-                    }).catch((err)=>{
-                        console.log(err)
-                    })
-                })
-
+            console.log(res.data)
+            if(res.data=='Success'){
+                this.setState({isOpen:false})
+                    toast.error(`Waiting Admin For Accepting Your Payment! THANKYOU`, {
+                        position: "top-right",
+                        autoClose: 2000,
+                        hideProgressBar: false,
+                        closeOnClick: true,
+                        draggable: true,
+                        progress: undefined,
+                                        });
+                                        this.setState({successful:true})
+            }
         }).catch((err)=>{
             console.log(err)
         })
-         
+
      }
+     
+     lunhAlgo=(answer)=>{
+        
+        var input = this.state.cc.current.value
+        var x = input
+    var counter = 0; //  ngitung total digit
+    var output1 = 0; // ngitung genap
+    var output2 = 0; // ngitung ganjil
+    var pecah = 0; // ngitung per digit, ngeluarin digit
+    do{
+        pecah = x%10
+        x = Math.floor(x/10)
+        counter++
+    }while (x>0)
+    
+    if (counter %2 == 0){
+        this.state.countergen = 1 
+    } else{
+        this.state.countergen = 0
+    }
+    var z = input
+    var a = Math.floor(z/Math.pow(10,15))
+    var hacep = 0
+    do{
+        hacep = z%10
+        z = Math.floor(z/10)
+        if (this.state.countergen%2 == 0){
+            var pecah2 = hacep*2
+            if (pecah2 >= 10){
+                var pecah3 = pecah2%10;
+                var pecah4 = Math.floor(pecah2/10)
+                output1 += pecah3+pecah4  
+            }else{
+                output1 += pecah2 //genap
+            }
+        } else if(this.state.countergen%2 != 0){
+            output2 += hacep //ganjil
+        }
+        this.state.countergen++
+    } while(z > 0)
+    var total = output1+output2
+    if(total % 10 == 0){
+        if(counter == 16){
+            if(a == 4) {
+                console.log('visa')
+                this.setState({ccName:'VISA'})
+                return `VISA`
+            }else {
+                console.log('master card')
+                this.setState({ccName:'Master Card'})
+                return `Master Card`
+            }
+        }else if (counter == 15){
+            console.log('amex')
+            this.setState({ccName:'AMEX'})
+            return `AMEX`
+        }else if (counter == 13){ 
+            console.log('visa')
+            this.setState({ccName:'VISA'})
+            return `VISA`
+        }
+    }else{
+        console.log('not valid')
+        this.setState({ccName:1})
+        this.setState({test:'BISA DONG CUK'})
+        // console.log(this.state.ccName)
+        // ccredux({cc:'not valid'})
+        return 'not valid'
+    }  
+
+    }
+    onbayarpakeCC=()=>{
+        this.lunhAlgo()
+        const result = this.lunhAlgo(this.result2)
+        console.log(result)
+        var sqlcart=this.state.sqlCart.map((val)=>{
+            return {
+                ProductId:val.ProductId,
+                harga:val.harga,
+                Qty:val.Qty
+            }
+        })
+        if(result ==='not valid'){
+            toast.error(`Wrong Number! Check Your Credit Card Number`, {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                draggable: true,
+                progress: undefined,
+            });
+
+        }else {
+            console.log('masuk pake cc')
+            Axios.post(`http://localhost:5001/cart/addNewTransactionsCC`,{
+                userId:this.props.id,
+            tanggalPembayaran:new Date().getTime(),
+            buktiPembayaran:this.state.cc.current.value,
+            sqlCart:sqlcart
+            }).then((res)=>{
+                if(res.data=='Success'){
+                    this.setState({isOpen:false})
+                        toast.error(`Payment Success! THANKYOU`, {
+                            position: "top-right",
+                            autoClose: 2000,
+                            hideProgressBar: false,
+                            closeOnClick: true,
+                            draggable: true,
+                            progress: undefined,
+                             });
+                    this.setState({successful:true})
+                }
+            }).catch((err)=>{
+                console.log(err)
+            })
+        }
+    }
+
+
+
+
+
+
 
     render() { 
+        
         if(this.state.successful){
             return <Redirect to='/'></Redirect>
         }
         if(this.props.role==='user'){
             console.log(this.state.sqlCart)
-            if(this.state.sqlCart==[]){
+            if(this.state.sqlCart.length===0){
                 return(
-                    <div>BELANJA SONO</div>
+                    <>
+                        <Header/>
+                        <div>
+                            <center>
+                                <div>
+                                    <h1>BELANJA DULU <br/>
+                                        BARU KESINI
+                                    </h1>
+                                </div>
+                            </center>
+                        </div>
+
+                    </>
                 )
                
             }else {
@@ -236,7 +343,7 @@ class Payment extends Component {
                                 }
                                 <div>
                                   {/* Total Harga  {priceFormatter(this.renderTotalHarga())} */}
-                                  Total Harga: {this.renderTotalHarga()}
+                                  Total Harga: {priceFormatter(this.renderTotalHarga())}
                                 </div>
                             </ModalBody>
                             <ModalFooter>
@@ -311,8 +418,7 @@ class Payment extends Component {
                                                     <p className="p-2">{priceFormatter(this.renderTotalHarga())}</p>
                                             </div>
                                             
-                                            <div className="cart-kanan-bawah-bwh" onClick={this.onCheckOutClick}>
-                                                
+                                            <div className="cart-kanan-bawah-bwh" onClick={this.onCheckOutClick}>           
                                                     <p>Beli(3)</p>                                                                                       
                                             </div>
                                     </div>
